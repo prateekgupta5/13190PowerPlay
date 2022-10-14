0fa2d63a7d652b25f0bec8157a53cb306d787ce2
wilcoxrobotics <teamrobotics8872@gmail.com>
2022-10-13 17:50:01 -0700

    added a basic trail to MeepMeep

--- MeepMeep/src/main/java/org/example/MeepMeepTesting.java
+++ MeepMeep/src/main/java/org/example/MeepMeepTesting.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
@@ -12,17 +13,18 @@ public class MeepMeepTesting {
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .build()
+                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(-34, -60, 0))
+                        .turn( Math.toRadians(90) )
+                        // trajrctory goes here
+                        //function pointers?
+                        //
+                        .forward(60)
+                        .turn( Math.toRadians(-90) )
+                        .waitSeconds(2)
+                        .lineToLinearHeading( new Pose2d(-56,-12, Math.toRadians(180) ) )
+                        .waitSeconds(2)
+                        .lineToLinearHeading( new Pose2d(-34,0, Math.toRadians(0) ) )
+                        .build()
                 );
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_OFFICIAL)
